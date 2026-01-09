c4034c8e87f2832652e88ad409fe42044455616c
Yasser <yasseretudes004@gmail.com>
2026-01-09 18:47:36 +0100

    Add redirects for SPA

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://parkflowbackend-production.up.railway.app",
+  baseURL: "https://park-flow-backend.onrender.com",
   withCredentials: true,
 });
 
